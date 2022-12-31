3f079a1c8b6822ac4cf14966ba28432fe7dc3dad
Ama Frema <frema90@gmail.com>
2022-12-30 21:51:22 -0500

    updated

---
+++ Main/public/js/profile.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#tech-name').value.trim();
+  const needed_funding = document.querySelector('#tech-funding').value.trim();
+  const description = document.querySelector('#tech-desc').value.trim();
+
+  if (name && needed_funding && description) {
+    const response = await fetch(`/api/techs`, {
+      method: 'POST',
+      body: JSON.stringify({ name, needed_funding, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create tech');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/techs/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete tech');
+    }
+  }
+};
+
+document
+  .querySelector('.new-tech-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.tech-list')
+  .addEventListener('click', delButtonHandler);
