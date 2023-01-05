fdcb6d41e6f32414d1fe264a88249f662d45cd73
Ama Frema <frema90@gmail.com>
2023-01-05 13:50:41 -0500

    updated

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const userRoutes = require('./userRoutes');
-const techRoutes = require('./techRoutes');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
-router.use('/techs', techRoutes);
+router.use('/techs', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
